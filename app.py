baced2590d484e3fdc2da44dbdb06636b0ca4bb8
Samuele  Chinellato <samuele.chinellato@bt.com>
2020-08-25 09:08:09 +0100

    OC CICD expose 8080

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def say_hello():
     return render_template('say-hello.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=8080)
