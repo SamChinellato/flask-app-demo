425c203c95b90d41e1c1a763897f0b8db76ec444
Samuele  Chinellato <samuele.chinellato@bt.com>
2020-09-08 13:43:17 +0100

    refactoring app.py

--- app.py
+++ app.py
@@ -5,28 +5,28 @@ this module."""
 
 from flask import Flask, render_template
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-@APP.route('/')
+@app.route('/')
 def home():
     """ Return the home template when given the '/' route """
     return render_template('home.html')
 
-@APP.route('/hello/')
-@APP.route('/hello/<name>')
+@app.route('/hello/')
+@app.route('/hello/<name>')
 def hello(name=None):
     """ Return the hello template when given the '/hello/' route """
     return render_template('hello.html', name=name)
 
-@APP.route('/about/')
+@app.route('/about/')
 def about():
     """ Return the about template when given the '/about/' route """
     return render_template('about.html')
 
-@APP.route('/say-hello')
+@app.route('/say-hello')
 def say_hello():
     """ Return the say-hello template when given the '/say-hello/' route """
     return render_template('say-hello.html')
 
 if __name__ == '__main__':
-    APP.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
