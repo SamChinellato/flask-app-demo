e80b316dbc6aa2a410b311e00201aef774cd38df
Samuele  Chinellato <samuele.chinellato@bt.com>
2020-09-08 14:19:00 +0100

    refactor for pycodestyle

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """ Return the home template when given the '/' route """
     return render_template('home.html')
+
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -18,15 +20,18 @@ def hello(name=None):
     """ Return the hello template when given the '/hello/' route """
     return render_template('hello.html', name=name)
 
+
 @app.route('/about/')
 def about():
     """ Return the about template when given the '/about/' route """
     return render_template('about.html')
 
+
 @app.route('/say-hello')
 def say_hello():
     """ Return the say-hello template when given the '/say-hello/' route """
     return render_template('say-hello.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
